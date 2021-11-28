db6221040b1fc8971d6beb108339992de91f4d32
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2021-11-27 21:49:04 -0300

    Resolução do primeiro exercício

---
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_primeiros_passos/exercicio_aula_course.js
@@ -0,0 +1,28 @@
+const a = 20;
+const b = 10;
+
+let qualOperacao = "Módulo";
+
+switch (qualOperacao) {
+
+    case "Adição":
+        console.log(a + b);
+        break;
+
+    case "Subtração":
+        console.log(a - b);
+        break;
+
+    case "Multiplicação":
+        console.log(a * b);
+        break;
+
+    case "Divisão":
+        console.log(a / b);
+        break;
+        
+    case "Módulo":
+        console.log(a % b);
+        break;
+
+}
