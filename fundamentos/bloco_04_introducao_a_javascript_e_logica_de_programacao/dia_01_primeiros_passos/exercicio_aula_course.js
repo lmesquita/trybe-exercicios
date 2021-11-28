10f586686c64599c0af635627a1f11b1a6133cc8
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2021-11-27 21:59:58 -0300

    Resolução do exercício 3

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_primeiros_passos/exercicio_aula_course.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_primeiros_passos/exercicio_aula_course.js
@@ -1,40 +1,56 @@
 const a = 20;
 const b = 10;
+const c = 10;
 
 /*  EXERCICIO 1    
 let qualOperacao = "Módulo";
 
 switch (qualOperacao) {
 
-    case "Adição":
-        console.log(a + b);
-        break;
+	case "Adição":
+			console.log(a + b);
+			break;
 
-    case "Subtração":
-        console.log(a - b);
-        break;
+	case "Subtração":
+			console.log(a - b);
+			break;
 
-    case "Multiplicação":
-        console.log(a * b);
-        break;
+	case "Multiplicação":
+			console.log(a * b);
+			break;
 
-    case "Divisão":
-        console.log(a / b);
-        break;
-        
-    case "Módulo":
-        console.log(a % b);
-        break;
+	case "Divisão":
+			console.log(a / b);
+			break;
+			
+	case "Módulo":
+			console.log(a % b);
+			break;
 
+}
+
+------------------------------
+
+/*  EXERCICIO 2     
+
+if(a < b){
+	console.log(b);
+	return b;
+}else{
+	console.log(a);
+	return a;
 }
 
 /*------------------------------*/
 
 
-if(a < b){
-    console.log(b);
-    return b;
-}else{
-    console.log(a);
-    return a;
+if ((a > b) && (a > c)) {
+	console.log(a);
+	return a;
+}else if ((b > a) && (b > c)) {
+	console.log(b);
+	return b;
+}else if ((c > a) && (c > b)) {
+	console.log(c);
+	return c;
 }
