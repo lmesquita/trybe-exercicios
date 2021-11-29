4a2a88c5e85d0cfa1eef5fad5527842608495ef5
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2021-11-29 20:35:27 -0300

    Resolução do exercício 2

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_array_e_loop_for/exercicios_aula_course.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_array_e_loop_for/exercicios_aula_course.js
@@ -1,5 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+/*---------- EXERCÍCIO 1 ---------- 
+
 for (let x =0; x < numbers.length; x++) {
     console.log(numbers[x]);
 }
+
+/*---------------------------------------- */
+
+//---------- EXERCÍCIO 2 ---------- 
+let soma =0;
+
+for (let x =0; x < numbers.length; x++) {
+    soma += numbers[x];
+}
+
+console.log("A soma dos números é:", soma);
+
+/*---------------------------------------- */
