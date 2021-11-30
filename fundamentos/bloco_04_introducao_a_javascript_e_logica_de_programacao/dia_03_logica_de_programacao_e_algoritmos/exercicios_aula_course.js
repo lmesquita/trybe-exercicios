e41b45c40a11bc2e7e8637c9d6dfee23c8359cd9
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2021-11-30 18:41:33 -0300

    Resolução do exercício 3

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_03_logica_de_programacao_e_algoritmos/exercicios_aula_course.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_03_logica_de_programacao_e_algoritmos/exercicios_aula_course.js
@@ -14,7 +14,7 @@ for (let i = 0; i < n; i++) {
 
 /*----------------------------------------*/
 
-/*---------- EXERCÍCIO 2 ----------*/
+/*---------- EXERCÍCIO 2 ----------
 
 let n = 5;
 let simbolo = '*';
@@ -23,6 +23,30 @@ let aux = '';
 for (let i = 0; i <= n; i++) {
 	console.log(aux);
 	aux += simbolo;
+}
+
+
+/*----------------------------------------*/
+
+/*---------- EXERCÍCIO 3 ----------*/
+
+let n = 5;
+let simbolo = '*';
+let aux = '';
+let posicao = n;
+
+for (let i = 0; i < n; i++) {
+	for (let j = 0; j <= n; j++) {
+		if (j < posicao) {
+			aux = aux + ' ';
+		} else {
+			aux = aux + simbolo;
+		}
+	}
+
+	console.log(aux);
+	aux = '';
+	posicao--;
 }
 
 
