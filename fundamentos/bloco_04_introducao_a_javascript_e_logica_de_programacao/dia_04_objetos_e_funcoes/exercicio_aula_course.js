8a593cadd415f71597759eebf114c5960292afc0
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2021-12-02 01:40:11 -0300

    Resolução do exercício 2

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_04_objetos_e_funcoes/exercicio_aula_course.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_04_objetos_e_funcoes/exercicio_aula_course.js
@@ -4,3 +4,17 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
+/*---------- EXERCÍCIO 1 ----------
+
+console.log("Bem vinda, " + info.personagem);
+
+/*----------------------------------------*/
+
+/*---------- EXERCÍCIO 2 ----------*/
+
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
+
+/*----------------------------------------*/
